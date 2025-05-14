9f321058938613f42ea67f0e68529dc6f3105033
Minsung <alstjd025@gmail.com>
2025-05-14 11:02:58 +0900

    Print fix
    Changes to be committed:
    	modified:   yonghwa/README.md
    	modified:   yonghwa/build.sh

--- yonghwa/build.sh
+++ yonghwa/build.sh
@@ -18,9 +18,9 @@ echo -e "${CYAN}Step 2: Granting execute permission...${RESET}"
 chmod +x bazel-bin/tensorflow/lite/examples/simple_test/simple_test
 
 echo -e "${CYAN}Step 3: Moving binary to target directory...${RESET}"
-mv bazel-bin/tensorflow/lite/examples/simple_test/simple_test /host_dir/LiteRT_benchmark/yongwha
+mv bazel-bin/tensorflow/lite/examples/simple_test/simple_test /host_dir/LiteRT_benchmark/yonghwa
 
 echo -e "${CYAN}Step 4: Changing directory to /host_home/LiteRT_benchmark...${RESET}"
-cd /host_dir/LiteRT_benchmark/yongwha || { echo -e "${RED}Failed to change directory.${RESET}"; exit 1; }
+cd /host_dir/LiteRT_benchmark/yonghwa || { echo -e "${RED}Failed to change directory.${RESET}"; exit 1; }
 
 echo -e "${GREEN}All tasks completed successfully.${RESET}"
